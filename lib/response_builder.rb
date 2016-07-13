effecceb50e7e659c92383bc341828b85f5ab390
Isaac Datlof <ike18t@gmail.com>
2016-07-13 12:36:30 -0400

    Add with_header and with_status_message to ResponseBuilder

--- lib/response_builder.rb
+++ lib/response_builder.rb
@@ -10,8 +10,19 @@ module WireMockMapper
       self
     end
 
+    def with_header(key, value)
+      @options['headers'] ||= {}
+      @options['headers'][key] = value
+      self
+    end
+
     def with_status(status_code)
       @options['status'] = status_code
+      self
+    end
+
+    def with_status_message(status_message)
+      @options['statusMessage'] = status_message
       self
     end
 
--- spec/response_builder_spec.rb
+++ spec/response_builder_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe WireMockMapper::ResponseBuilder do
-  context 'with_body' do
+  describe 'with_body' do
     it 'adds the body' do
       builder = WireMockMapper::ResponseBuilder.new
       builder.with_body('some body')
@@ -17,12 +17,38 @@ describe WireMockMapper::ResponseBuilder do
     end
   end
 
-  context 'with_status' do
+  describe 'with_header' do
+    it 'adds the header' do
+      builder = WireMockMapper::ResponseBuilder.new
+      builder.with_header('key', 'value')
+      result = builder.to_hash
+      expect(result['headers']).to eq('key' => 'value')
+    end
+
+    it 'adds multiple headers' do
+      builder = WireMockMapper::ResponseBuilder.new
+      builder.with_header('key', 'value')
+      builder.with_header('another key', 'another value')
+      result = builder.to_hash
+      expect(result['headers']).to eq('key' => 'value', 'another key' => 'another value')
+    end
+  end
+
+  describe 'with_status' do
     it 'adds the status code' do
       builder = WireMockMapper::ResponseBuilder.new
       builder.with_status(400)
       result = builder.to_hash
       expect(result['status']).to eq(400)
+    end
+  end
+
+  describe 'with_status_message' do
+    it 'adds the status message' do
+      builder = WireMockMapper::ResponseBuilder.new
+      builder.with_status_message('message')
+      result = builder.to_hash
+      expect(result['statusMessage']).to eq('message')
     end
   end
 end
