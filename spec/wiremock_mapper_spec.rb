c500d1375c6feec09f717355fabddd5473e11f32
James Brechtel <jbrechtel@gmail.com>
2017-05-24 16:21:58 -0400

    Add method to delete created mappings so they can be flushed after tests.

--- spec/wiremock_mapper_spec.rb
+++ spec/wiremock_mapper_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe WireMockMapper do
+  before(:each) { WireMockMapper.send(:forget_saved_mappings) }
   describe 'create_mapping' do
     it 'posts the correct json to the wiremock url' do
       url = 'http://nowhere.com'
@@ -11,7 +12,7 @@ describe WireMockMapper do
                                              { 'matches' => 'some request body' }
                                            ] },
                                 response: { 'body' => 'some response body' } }
-      stub_request(:post, "#{url}/__admin/mappings/new").with(body: expected_request_body)
+      stub_request(:post, "#{url}/__admin/mappings/new").with(body: expected_request_body).to_return(body: '{}')
 
       WireMockMapper.create_mapping(url) do |request, respond|
         request.is_a_post
@@ -34,7 +35,7 @@ describe WireMockMapper do
                                              { 'matches' => 'some request body' }
                                            ] },
                                 response: { 'body' => 'some response body' } }
-      stub_request(:post, "#{url}/__admin/mappings/new").with(body: expected_request_body)
+      stub_request(:post, "#{url}/__admin/mappings/new").with(body: expected_request_body).to_return(body: '{}')
 
       request_builder = WireMockMapper::Builders::RequestBuilder.new.with_header('some_global_header').equal_to('some global header value')
       expect(WireMockMapper::Configuration).to receive(:request_builder).and_return(request_builder)
@@ -49,5 +50,38 @@ describe WireMockMapper do
       end
     end
     # rubocop:enable Metrics/LineLength
+  end
+
+  describe 'delete_mappings' do
+    it 'issues a DELETE for each created mapping' do
+      mapping_id = 'james-james-james'
+      url = 'http://nowhere.com'
+      response_body = { id: mapping_id }.to_json
+      mapping_path = "#{url}/__admin/mappings/#{mapping_id}"
+
+      stub_request(:post, "#{url}/__admin/mappings/new").to_return(body: response_body)
+
+      WireMockMapper.create_mapping(url) {}
+      stub_request(:delete, mapping_path)
+      WireMockMapper.delete_mappings(url)
+
+      assert_requested(:delete, mapping_path)
+    end
+
+    it "does not delete the same mappings once they've been deleted" do
+      mapping_id = 'james-james-james'
+      url = 'http://nowhere.com'
+      response_body = { id: mapping_id }.to_json
+      mapping_path = "#{url}/__admin/mappings/#{mapping_id}"
+
+      stub_request(:post, "#{url}/__admin/mappings/new").to_return(body: response_body)
+
+      WireMockMapper.create_mapping(url) {}
+      stub_request(:delete, mapping_path)
+      WireMockMapper.delete_mappings(url)
+      WireMockMapper.delete_mappings(url)
+
+      assert_requested(:delete, mapping_path, times: 1)
+    end
   end
 end
