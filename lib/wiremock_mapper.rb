a75fb3fa22d859a23db3e87e1acde96b2578f29b
Jarod Adair <jarod.adair@manheim.com>
2017-10-18 13:15:11 -0500

    adding scenario builder

--- lib/wiremock_mapper.rb
+++ lib/wiremock_mapper.rb
@@ -6,10 +6,13 @@ module WireMockMapper
     def create_mapping(url = Configuration.wiremock_url)
       request_builder = deep_clone(Configuration.request_builder)
       response_builder = deep_clone(Configuration.response_builder)
+      scenario_builder = deep_clone(Configuration.scenario_builder)
 
-      yield request_builder, response_builder
+      yield request_builder, response_builder, scenario_builder
 
-      response = send_to_wiremock(url, request: request_builder, response: response_builder)
+      body = {request: request_builder, response: response_builder}.merge(scenario_builder)
+      response = send_to_wiremock(url, body)
+
       JSON.parse(response.body).fetch('id')
     end
 
@@ -21,10 +24,15 @@ module WireMockMapper
       clear_wiremock_mappings(url)
     end
 
+    def reset_scenarios(url = Configuration.wiremock_url)
+      reset_wiremock_scenarios(url)
+    end
+
     private
 
     WIREMOCK_MAPPINGS_PATH = '__admin/mappings'.freeze
     WIREMOCK_CLEAR_MAPPINGS_PATH = "#{WIREMOCK_MAPPINGS_PATH}/reset".freeze
+    WIREMOCK_RESET_SCENARIOS_PATH = '__admin/scenarios/reset'.freeze
 
     def deep_clone(object)
       Marshal.load(Marshal.dump(object))
@@ -49,6 +57,13 @@ module WireMockMapper
       http = Net::HTTP.new(uri.host, uri.port)
       request = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
       request.body = body.to_json
+      http.request(request)
+    end
+
+    def reset_wiremock_scenarios(url)
+      uri = URI([url, WIREMOCK_RESET_SCENARIOS_PATH].join('/'))
+      http = Net::HTTP.new(uri.host, uri.port)
+      request = Net::HTTP::Post.new(uri.path)
       http.request(request)
     end
   end
---
+++ spec/scenario_builder_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe WireMockMapper::Builders::ScenarioBuilder do
+  let(:builder) { WireMockMapper::Builders::ScenarioBuilder.new }
+
+  describe 'in_scenario' do
+    it 'sets the scenario name' do
+      builder.in_scenario('some scenario')
+      result = builder.to_hash
+      expect(result[:scenarioName]).to eq('some scenario')
+    end
+  end
+
+  describe 'with_state' do
+    it 'sets the required state of the request' do
+      builder.with_state('initial state')
+      result = builder.to_hash
+      expect(result[:requiredScenarioState]).to eq('initial state')
+    end
+  end
+
+  describe 'will_set_state' do
+    it 'sets the state value to be toggled to' do
+      builder.will_set_state('updated state')
+      result = builder.to_hash
+      expect(result[:newScenarioState]).to eq('updated state')
+    end
+  end
+
+end
